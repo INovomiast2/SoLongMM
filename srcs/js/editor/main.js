12b9c89ce9837e34852cb628e166bba98ac3a863
INovomiast2 <idimnovdie1602@protonmail.com>
2024-03-10 15:04:53 +0100

    CHANGES:     added new.js     modified editor.css     modified index.html     modified main.js
        -- COMMIT MESSAGE --
        added new.js
        modified editor.css

--- srcs/js/editor/main.js
+++ srcs/js/editor/main.js
@@ -4,7 +4,8 @@ setTimeout(() => {
     Promise.all([
         import('./map.js'),
         import('./generate.js'),
-        import('./save.js')
+        import('./save.js'),
+        import('./new.js')
       ]).then(() => {
         isEditorLoading = false;
         document.querySelector('.editor-loading-container').style.display = 'none';
@@ -13,9 +14,107 @@ setTimeout(() => {
 }, 3500);
 
 // Handling sidebar tiles
-let tiles = ['https://streak.club/img/Mix1c2VyX2NvbnRlbnQvdXBsb2Fkcy9pbWFnZS8zMjYzMi5wbmc=/600x600%23/gvhkAS.png', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnQ5wiJVLhyv_DAmLzqj111mcH1SErkztkHw&usqp=CAU']; // Assuming these should be paths to tile images
+// Get the tiles from localStorage
+let tiles = JSON.parse(localStorage.getItem('currentProject')?.tiles) || [];
 
 const tileContainer = document.getElementById('sidebar-tiles');
+// If there are no tiles, display a button to import tiles
+if (tiles.length === 0) {
+  tileContainer.innerHTML = `
+    <button id="import-tiles-btn">Import Tiles</button>
+  `;
+
+  document.getElementById('import-tiles-btn').addEventListener('click', () => {
+    // Open a modal with options of using a file input or a URL input
+    const modal = document.createElement('dialog');
+    modal.id = 'modal';
+    modal.innerHTML = `
+      <form id="import-tiles-form">
+        <label for="import-tiles-file">Import Tiles</label>
+        <input type="file" id="import-tiles-file" accept="image/*" required>
+        <button type="submit">Import</button>
+      </form>
+      <hr style="">
+      <form id="import-tiles-form-url">
+        <label for="import-tiles-url">Import Tiles</label>
+        <input type="url" id="import-tiles-url" required>
+        <button type="submit" id="send-url">Import</button>
+    `;
+    // Modal Style
+    modal.style.width = '500px';
+    modal.style.height = '550px';
+    modal.style.position = 'absolute';
+    modal.style.top = '50%';
+    modal.style.left = '50%';
+    modal.style.transform = 'translate(-50%, -50%)';
+    modal.style.backgroundColor = '#242424';
+    modal.style.color = 'white';
+    modal.style.borderRadius = '10px';
+    modal.style.border = '1px solid black';
+    modal.style.padding = '20px';
+    modal.style.boxShadow = '0px 0px 10px 0px black';
+    modal.style.overflowY = 'none';
+    // Form Style
+    const form = modal.querySelectorAll('form');
+    form.forEach(f => {
+      f.style.display = 'flex';
+      f.style.flexDirection = 'column';
+      f.style.alignItems = 'center';
+      f.style.justifyContent = 'center';
+      f.style.width = '100%';
+      f.style.height = '50%';
+    });
+    // Input Style
+    const input = modal.querySelectorAll('input');
+    input.forEach(i => {
+      i.style.width = '100%';
+      i.style.marginBottom = '20px';
+      i.style.padding = '10px';
+      i.style.border = 'none';
+      i.style.borderRadius = '10px';
+      i.style.backgroundColor = '#333333';
+      i.style.color = 'white';
+      i.style.fontSize = '18px';
+      i.style.fontFamily = 'Poppins, sans-serif';
+      i.style.textAlign = 'center';
+      i.style.marginTop = '10px';
+    });
+    // Button Style
+    const button = modal.querySelectorAll('button');
+    button.forEach(b => {
+      b.style.width = '50%';
+      b.style.height = '50px';
+    });
+    document.body.appendChild(modal);
+    modal.showModal();
+
+    // Form Submit URL
+    document.getElementById('import-tiles-form-url').addEventListener('submit', e => {
+      e.preventDefault();
+      const url = document.getElementById('import-tiles-url').value;
+      // Fetch the image and add it to the tiles array
+      fetch(url)
+        .then(response => response.blob())
+        .then(blob => {
+          const reader = new FileReader();
+          reader.onload = () => {
+            tiles.push(reader.result);
+            localStorage.setItem('tiles', JSON.stringify(tiles));
+            modal.close();
+          };
+          reader.readAsDataURL(blob);
+          // Remove the button and append the tiles
+          tileContainer.innerHTML = '';
+          tiles.forEach(tilePath => {
+            const tileElement = createTileElement(tilePath);
+            tileContainer.appendChild(tileElement);
+          });
+          
+        });
+    });
+  });
+}
+
 
 // Check if tiles have already been appended to prevent duplication
 // We check for a specific class that we add to our tiles
@@ -56,5 +155,14 @@ document.querySelectorAll('.editor-sidebar-section-tile img').forEach(tile => {
     // Then, add 'active' class to the clicked tile
     tile.classList.add('active');
     tile.dataset.active = "true";
+    // Add current tile id to localStorage for later use
+    localStorage.setItem('currentTile', tile.id);
   });
 });
+
+
+// Check currentProject is set in localStorage and it's not empty and if not, take the currentProject name and display it in the title of the window
+const currentProject = JSON.parse(localStorage.getItem('currentProject'));
+if (currentProject && currentProject.name) {
+  document.title = `${currentProject.name} - Editor - So Long MM`;
+}
---
+++ srcs/js/editor/new.js
@@ -0,0 +1,118 @@
+/* New Project */
+const newProject = document.getElementById('header-editor-new-prj');
+
+newProject.addEventListener('click', () => {
+    // Create a modal to get the project name
+    const modal = document.createElement('dialog');
+    modal.id = 'modal';
+    modal.innerHTML = `
+        <form id="new-project-form">
+            <label for="project-name">Project Name</label>
+            <input type="text" id="project-name" required>
+            <!-- Add width and height for the map -->
+            <label for="map-width">Map Width</label>
+            <input type="number" id="map-width" required>
+            <label for="map-height">Map Height</label>
+            <input type="number" id="map-height" required>
+            <br>
+            <button type="submit">Create</button>
+        </form>
+    `;
+    // Modal Style
+    modal.style.width = '500px';
+    modal.style.height = '400px';
+    modal.style.position = 'absolute';
+    modal.style.top = '50%';
+    modal.style.left = '50%';
+    modal.style.transform = 'translate(-50%, -50%)';
+    modal.style.backgroundColor = '#242424';
+    modal.style.color = 'white';
+    modal.style.borderRadius = '10px';
+    modal.style.border = '1px solid black';
+    modal.style.padding = '20px';
+    modal.style.boxShadow = '0px 0px 10px 0px black';
+    // Form Style
+    const form = modal.querySelector('form');
+    form.style.display = 'flex';
+    form.style.flexDirection = 'column';
+    form.style.alignItems = 'center';
+    form.style.justifyContent = 'center';
+    form.style.width = '100%';
+    form.style.height = '100%';
+    // Input Style
+    const input = form.querySelectorAll('input');
+    input.forEach(i => {
+        i.style.width = '100%';
+        i.style.marginBottom = '20px';
+        i.style.padding = '10px';
+        i.style.border = 'none';
+        i.style.borderRadius = '10px';
+        i.style.backgroundColor = '#333333';
+        i.style.color = 'white';
+        i.style.fontSize = '18px';
+        i.style.fontFamily = 'Poppins, sans-serif';
+        i.style.textAlign = 'center';
+        i.style.marginTop = '10px';
+        i.min = 1;
+        i.max = 100;
+    });
+    // Button Style
+    const button = form.querySelector('button');
+    button.style.width = '50%';
+    button.style.height = '50px';
+    button.style.backgroundColor = 'royalblue';
+    button.style.color = 'white';
+    button.style.border = 'none';
+    button.style.borderRadius = '10px';
+    button.style.cursor = 'pointer';
+    // Form Submit
+    document.body.appendChild(modal);
+    modal.showModal();
+    // Form Submit
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        const projectName = document.getElementById('project-name').value;
+        const mapWidth = document.getElementById('map-width').value;
+        const mapHeight = document.getElementById('map-height').value;
+        // Save the project data on a object to save it on the localStorage
+        const newProject = {
+            name: projectName,
+            width: mapWidth,
+            height: mapHeight,
+            tiles: []
+        };
+
+        // Save the project on the localStorage
+        localStorage.setItem('currentProject', JSON.stringify(newProject));
+        // Close the modal
+        modal.remove();
+
+        // Create a div with the grid to display the tiles
+        const grid = document.createElement('div');
+        grid.id = 'grid';
+        grid.style.display = 'grid';
+        grid.style.gridTemplateColumns = `repeat(${mapWidth}, 1fr)`;
+        grid.style.gridTemplateRows = `repeat(${mapHeight}, 1fr)`;
+        grid.style.width = '100%';
+        grid.style.height = '100%';
+        grid.style.position = 'absolute';
+        grid.style.top = '50%';
+        // Add buttons to add and remove tiles
+        const addTile = document.createElement('button');
+        addTile.id = 'add-tile';
+        addTile.textContent = 'Add Tile';
+        addTile.style.position = 'absolute';
+        addTile.style.top = '10px';
+        addTile.style.right = '10px';
+        addTile.style.backgroundColor = 'royalblue';
+        addTile.style.color = 'white';
+        addTile.style.border = 'none';
+        addTile.style.borderRadius = '10px';
+
+        // Change the title of page to the project name
+        document.title = `${projectName} - Editor - So Long MM`;
+
+        // Add the grid to the body
+        document.body.appendChild(grid);
+    });
+});
